787f17702e4da1b4757b9c274a895b2463e1826d
OlgaChusheva <helga_ever@mail.ru>
2023-02-18 22:18:05 +0300

    Initial Commit

---
+++ counterApp/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  counterApp
+//
+//  Created by Ольга Чушева on 12.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var numberLabel: UILabel!
+    @IBOutlet weak var clickButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+       
+    }
+var click = 0
+    @IBAction func buttonDidTap(_ sender: Any) {
+        click = click + 1
+        numberLabel.text = "Значение счетчика: \(String(click))"
+    }
+    
+}
+
